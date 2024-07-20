a17610ef092208914f65abe4f5109a59cf6b2876
Aboubakar <aboubakar.khan14@gmail.com>
2024-07-20 14:45:44 +0200

    Initialize each AI models at API startup

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, FastAPI
 
 from src.routers import diagram
+from src.handlers.Factory import Factory
 
 app = FastAPI()
 
@@ -10,6 +11,18 @@ api_router = APIRouter(prefix="/api")
 api_router.include_router(diagram.router)
 
 app.include_router(api_router)
+
+
+@app.on_event("startup")
+async def startup_event():
+    """
+    This function is called when the API starts.
+
+    It initializes the API by calling the `initialize` method of the `Factory` class
+    which initializes all the models defined in the configuration file.
+    """
+    print("Initializing the API...")
+    Factory.initialize_models()
 
 
 @app.get("/")
