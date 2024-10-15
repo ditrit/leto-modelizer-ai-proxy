1fd08e13582d805c504ef9dcbca8dfe670a4d623
Aboubakar <aboubakar.khan14@gmail.com>
2024-10-15 14:42:19 +0200

    Add minimalist health endpoint

--- tests/unit/routers/test_main.py
+++ tests/unit/routers/test_main.py
@@ -9,3 +9,9 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello From Leto-Modelizer-AI-API!"}
+
+
+def test_read_health():
+    response = client.get("health/")
+    assert response.status_code == 200
+    assert response.json()["status"] == "ok"
