d79c44bc93e6862eb688131331f4c865e9f418af
Aboubakar <aboubakar.khan14@gmail.com>
2024-11-25 15:24:18 +0100

    Update BaseHandler: The handler should use initialize_configuration before usage

--- src/handlers/BaseHandler.py
+++ src/handlers/BaseHandler.py
@@ -1,3 +1,6 @@
+import os
+import json
+import inspect
 from abc import ABC, abstractmethod
 
 from src.configuration.configurationManager import ConfigurationManager
@@ -18,9 +21,10 @@ class BaseHandler(ABC):
             ai_name (str): The name of the AI model to use for the handler.
         """
         self.ai_name = ai_name
-        self.configuration = ConfigurationManager().get_configuration()["ai-models"][
-            ai_name
-        ]
+        self.configuration = None
+
+    def initialize_configuration(self):
+        self.configuration = ConfigurationManager().get_configuration()[self.ai_name]
 
     @abstractmethod
     def initialize(self):
@@ -49,3 +53,30 @@ class BaseHandler(ABC):
             message (Message): The message object containing the message to send to the AI.
         """
         pass
+
+    def get_configuration_description(self, file_name: str = None):
+        """
+        Returns the description of the configuration fields that are used by the handler.
+
+        Parameters:
+            file_name (str, optional): The name of the file to get the description of.
+            If None, the description of the default file is used (description.json).
+
+        Returns:
+            str: The description of the file.
+        """
+        # Get the directory of the current class (child class)        # Get the file path of the child class
+        child_class_file = inspect.getfile(self.__class__)
+
+        # Get the directory where the child class file is located
+        child_class_dir = os.path.dirname(os.path.abspath(child_class_file))
+
+        file_name = file_name or "configuration_description.json"
+        json_file_path = os.path.join(child_class_dir, file_name)
+        # Read the JSON file and return its contents
+        try:
+            with open(json_file_path, "r") as file:
+                description = json.load(file)
+            return description
+        except FileNotFoundError:
+            return "JSON file not found in the class's folder."
