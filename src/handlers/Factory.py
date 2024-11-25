eb4d4aae5ee68e54220fd65c043df46e1234b2d8
Aboubakar <aboubakar.khan14@gmail.com>
2024-11-25 15:27:44 +0100

    Update Factory to handle new configuration

--- src/handlers/Factory.py
+++ src/handlers/Factory.py
@@ -5,8 +5,18 @@ from src.handlers.Gemini.GeminiHandler import GeminiHandler
 
 class Factory:
     """
-    Factory class for creating adequate handler objects.
+    Factory class for handling adequate handler objects.
     """
+
+    @staticmethod
+    def get_all_handlers():
+        """
+        Retrieves all the names of the handlers that are available.
+
+        Returns:
+            dict: A dictionary with the handler names as keys and the handler objects as values.
+        """
+        return {"ollama": OllamaHandler(), "gemini": GeminiHandler()}
 
     @staticmethod
     def get_handler(plugin_name: str):
@@ -19,24 +29,54 @@ class Factory:
         Returns:
             OllamaHandler: The handler object for the specified plugin name, or None if the plugin name is not found or the configuration value is not "ollama".
         """
-        configuration = ConfigurationManager().get_configuration()
 
-        plugin_name = configuration["pluginPreferences"].get(
-            plugin_name
-        ) or configuration["pluginPreferences"].get("default")
+        configuration_manager = ConfigurationManager()
 
-        if plugin_name == "ollama":
-            return OllamaHandler()
-        if plugin_name == "gemini":
-            return GeminiHandler()
+        # Retrieve the configuration after ensuring it is set
+        configuration = configuration_manager.get_configuration()
+
+        handler_name = (
+            configuration.get("plugin", {}).get("preferences", {}).get(plugin_name)
+            or configuration.get("plugin", {}).get("preferences", {}).get("default")
+            or "ollama"
+        )
+        handler = Factory.get_all_handlers().get(handler_name)
+        handler.initialize_configuration()
+
+        return handler
 
     @staticmethod
-    def initialize_models():
+    def initialize_models(handlers: set[str] | None = None):
         """
         Initializes all the models defined in the configuration file.
-        """
-        configuration = ConfigurationManager().get_configuration()
 
-        for models in configuration["ai-models"]:
-            handler = Factory.get_handler(models)
-            handler.initialize()
+        Parameters:
+            handlers (set[str] | None, optional): The handler names to initialize. Defaults to None.
+        """
+        responses = []
+        if handlers:
+            for handler in handlers:
+                handler_instance = Factory.get_handler(handler)
+                if handler_instance:
+                    handler_instance.initialize_configuration()
+                    responses.append(handler_instance.initialize())
+        else:
+            for handler_instance in Factory.get_all_handlers().values():
+                handler_instance.initialize_configuration()
+                responses.append(handler_instance.initialize())
+        return responses
+
+    @staticmethod
+    def get_all_configuration_descriptions():
+        """
+        Retrieves all the descriptions of the configuration fields used by all the handlers.
+
+        Returns:
+            dict: A dictionary with the handler names as keys and the descriptions as values.
+        """
+        configuration_description = {}
+        for ai_name, handler in Factory.get_all_handlers().items():
+            description = handler.get_configuration_description()
+            configuration_description[ai_name] = description
+
+        return configuration_description
--- tests/unit/handlers/test_Factory.py
+++ tests/unit/handlers/test_Factory.py
@@ -1,9 +1,17 @@
 import pytest
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock
 
+from src.configuration.configurationManager import ConfigurationManager
 from src.handlers.Factory import Factory
 from src.handlers.Ollama.OllamaHandler import OllamaHandler
 from src.handlers.Gemini.GeminiHandler import GeminiHandler
+
+
+def test_get_all_handlers():
+    all_handlers = Factory.get_all_handlers()
+    assert type(all_handlers) == dict
+    assert type(all_handlers["ollama"]) == OllamaHandler
+    assert type(all_handlers["gemini"]) == GeminiHandler
 
 
 @pytest.mark.parametrize(
@@ -12,13 +20,18 @@ from src.handlers.Gemini.GeminiHandler import GeminiHandler
         (
             "default",
             {
-                "pluginPreferences": {"default": "ollama"},
-                "ai-models": {
-                    "ollama": {
-                        "base_url": "http://localhost:11434/api",
-                        "models": ["mistral"],
-                        "defaultModel": "mistral",
-                        "modelFiles": ["default-mistral-modelfile"],
+                "plugin": {"preferences": {"default": "ollama"}},
+                "ollama": {
+                    "base_url": "http://localhost:11434/api",
+                    "models": ["mistral"],
+                    "defaultModel": "mistral",
+                    "modelFiles": {
+                        "generate": {
+                            "default": 'FROM mistral SYSTEM """ test generate """'
+                        },
+                        "message": {
+                            "default": 'FROM mistral SYSTEM """ test message """'
+                        },
                     },
                 },
             },
@@ -27,22 +40,16 @@ from src.handlers.Gemini.GeminiHandler import GeminiHandler
         (
             "default",
             {
-                "pluginPreferences": {"default": "gemini"},
-                "ai-models": {
-                    "gemini": {
-                        "base_url": "http://localhost:11434/api",
-                        "key": "key",
-                        "system_instruction": {
-                            "generate": {
-                                "default": "default-generate.json",
-                                "@ditrit/kubernator-plugin": "default-kubernetes.json",
-                                "@ditrit/githubator-plugin": "default-githubactions.json",
-                            },
-                            "message": {
-                                "default": "default-message.json",
-                                "@ditrit/kubernator-plugin": "default-kubernetes.json",
-                                "@ditrit/githubator-plugin": "default-githubactions.json",
-                            },
+                "plugin": {"preferences": {"default": "gemini"}},
+                "gemini": {
+                    "base_url": "http://localhost:11434/api",
+                    "key": "key",
+                    "system_instruction": {
+                        "generate": {
+                            "default": '{"system_instruction":{"parts":{"text": "test"}}}'
+                        },
+                        "message": {
+                            "default": '{"system_instruction":{"parts":{"text": "test2"}}}'
                         },
                     },
                 },
@@ -51,37 +58,77 @@ from src.handlers.Gemini.GeminiHandler import GeminiHandler
         ),
     ],
 )
-def test_get_handlers(plugin_name, configuration, expected_ai):
+def test_get_handler(plugin_name, configuration, expected_ai):
 
-    with patch(
-        "src.configuration.configurationManager.ConfigurationManager.get_configuration"
-    ) as mock_get_configuration:
-        mock_get_configuration.return_value = configuration
+    # Mock the get_configuration function
+    config_manager1 = ConfigurationManager()
+    config_manager1.get_configuration = MagicMock()
+    config_manager1.get_configuration.return_value = configuration
 
-        assert type(Factory.get_handler(plugin_name)) == expected_ai
-
-
-def test_intialize_models():
-
-    fake_configuration = {
-        "pluginPreferences": {"default": "fakeAI"},
-        "ai-models": {
-            "fakeAI": {
-                "base_url": "http://localhost:11434/api",
-                "models": ["mistral"],
-                "defaultModel": "mistral",
-                "modelFiles": ["default-mistral-modelfile"],
-            },
-        },
+    # Mock the get_all_handlers function in order to avoir calling initialize_configuration
+    mocked_ollama_handler = OllamaHandler()
+    mocked_ollama_handler.initialize_configuration = MagicMock()
+    mocked_gemini_handler = GeminiHandler()
+    mocked_gemini_handler.initialize_configuration = MagicMock()
+    Factory.get_all_handlers = MagicMock()
+    Factory.get_all_handlers.return_value = {
+        "ollama": mocked_ollama_handler,
+        "gemini": mocked_gemini_handler,
     }
 
-    with patch(
-        "src.configuration.configurationManager.ConfigurationManager.get_configuration"
-    ) as mock_get_configuration:
-        mock_get_configuration.return_value = fake_configuration
+    assert type(Factory.get_handler(plugin_name)) == expected_ai
 
-        mocked_handler = MagicMock()
-        Factory.get_handler = MagicMock()
-        Factory.get_handler.return_value = mocked_handler
-        Factory.initialize_models()
-        assert mocked_handler.initialize.call_count == 1
+
+def test_intialize_models_with_given_handlers():
+
+    mocked_handler = MagicMock()
+    mocked_handler.initialize_configuration = MagicMock()
+    mocked_handler.initialize.return_value = "response1"
+
+    mocked_handler2 = MagicMock()
+    mocked_handler2.initialize_configuration = MagicMock()
+    mocked_handler2.initialize.return_value = "response2"
+
+    Factory.get_handler = MagicMock()
+    Factory.get_handler.side_effect = [mocked_handler, mocked_handler2]
+    res = Factory.initialize_models(["fake_handler", "fake_handler2"])
+    assert mocked_handler.initialize.call_count == 1
+    assert mocked_handler2.initialize.call_count == 1
+    assert res == ["response1", "response2"]
+
+
+def test_intialize_all_models():
+
+    # Mock the get_all_handlers function in order to avoir calling initialize_configuration
+    mocked_ollama_handler = OllamaHandler()
+    mocked_ollama_handler.initialize_configuration = MagicMock()
+    mocked_ollama_handler.initialize = MagicMock()
+    mocked_ollama_handler.initialize.return_value = "response1"
+    mocked_gemini_handler = GeminiHandler()
+    mocked_gemini_handler.initialize_configuration = MagicMock()
+    mocked_gemini_handler.initialize = MagicMock()
+    mocked_gemini_handler.initialize.return_value = "response2"
+    Factory.get_all_handlers = MagicMock()
+    Factory.get_all_handlers.return_value = {
+        "ollama": mocked_ollama_handler,
+        "gemini": mocked_gemini_handler,
+    }
+
+    res = Factory.initialize_models()
+    assert res == ["response1", "response2"]
+
+
+def test_get_all_configuration_descriptions():
+    descriptions = Factory.get_all_configuration_descriptions()
+
+    assert "ollama" in descriptions
+    assert descriptions["ollama"][0]["key"] == "base_url"
+    assert descriptions["ollama"][1]["defaultValue"] == "mistral"
+    assert descriptions["ollama"][2]["key"] == "allowRawResults"
+    assert descriptions["ollama"][3]["type"] == "textarea"
+
+    assert "gemini" in descriptions
+    assert descriptions["gemini"][0]["key"] == "base_url"
+    assert descriptions["gemini"][1]["defaultValue"] == ""
+    assert descriptions["gemini"][2]["key"] == "system_instruction.generate.default"
+    assert descriptions["gemini"][3]["type"] == "textarea"
